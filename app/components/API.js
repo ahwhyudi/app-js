3fc1de9c0bd3383b76c685327fa46558821bd380
ahwhyudi <ahmad.wahyudi0501@gmail.com>
2025-07-15 23:13:54 +0700

    fix

--- app/components/API.js
+++ app/components/API.js
@@ -1,3 +1,3 @@
-const HOST = "https://c29d451a1bf3.ngrok-free.app/";
-export const BASE_URL = `${HOST}api/`;
-export const STORAGE_URL = `${HOST}storage/`;
+const HOST = "https://22ebc9de0f56.ngrok-free.app";
+export const BASE_URL = `${HOST}/api/`;
+export const STORAGE_URL = `${HOST}/storage/`;
